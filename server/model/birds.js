d1fd0a2b29ecc8dda613b2afea8f045517a639f1
MansourMina <mansourmina10@gmail.com>
2022-01-20 13:12:49 +0100

    now done

--- server/model/birds.js
+++ server/model/birds.js
@@ -2,14 +2,10 @@ const birds = require('../birds.json');
 
 const getBirds = () => birds;
 const updateBird = (id, body) => {
-  // eslint-disable-next-line consistent-return
-  birds.forEach((el, i) => {
-    if (el.id === Number(id)) {
-      birds[i].count = el.count + 1;
-      birds[i].observer.push(body.name);
-    }
-  });
-  return birds.find((el) => el.id === Number(id));
+  const bird = birds.find((el) => el.id === Number(id));
+  bird.count += 1;
+  bird.observer.push(body.name);
+  return bird;
 };
 
 module.exports = { getBirds, updateBird };
